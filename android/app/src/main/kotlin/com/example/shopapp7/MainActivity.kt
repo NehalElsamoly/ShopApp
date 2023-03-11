ba69982b24bbad227fb005afdd4f98c29525b16c
nehal <nehalelsamoly123@gmial.com>
2023-03-11 22:55:03 +0200

    ShopApp

---
+++ android/app/src/main/kotlin/com/example/shopapp7/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shopapp7
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/modules/shopapp/favourite/favourite.dart
@@ -0,0 +1,31 @@
+import 'package:conditional_builder/conditional_builder.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:udemy_flutter/layout/cubit/cubit.dart';
+import 'package:udemy_flutter/shared/components/components.dart';
+import 'package:udemy_flutter/shared/styles/colors.dart';
+
+import '../../../layout/cubit/states.dart';
+
+class FavoritesScreen extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return BlocConsumer<ShopCubit, ShopStates>(
+      listener: (context, state) {},
+      builder: (context, state)
+      {
+        return ConditionalBuilder(
+          condition: state is! ShopLoadingGetFavoritesState,
+          builder: (context) => ListView.separated(
+            itemBuilder: (context, index) => buildListProduct(ShopCubit.get(context).favoritesModel.data.data[index].product, context),
+            separatorBuilder: (context, index) => myDivider(),
+            itemCount: ShopCubit.get(context).favoritesModel.data.data.length,
+          ),
+          fallback: (context) => Center(child: CircularProgressIndicator()),
+        );
+      },
+    );
+  }
+}
